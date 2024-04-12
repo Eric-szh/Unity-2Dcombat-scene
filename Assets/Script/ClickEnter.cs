ce6966c745e063e8bfb9e35ed69b2b51ae9bd0cd
Eric-szh <Eric.szh@hotmail.com>
2024-04-12 13:15:40 -0400

    bgm bug

--- Assets/Script/ClickEnter.cs
+++ Assets/Script/ClickEnter.cs
@@ -9,7 +9,7 @@ public class ClickEnter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        bgmObject = GameObject.Find("BGM");
     }
 
     // Update is called once per frame
